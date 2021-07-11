67f9532f10306a0d12233cecac5cc9bda3dee729
roman-azami <nascentsoul13@gmail.com>
2021-07-12 00:50:42 +0300

    Add export playlists endpoint

--- src/main/kotlin/com/dewy/musicextractish/spotify/SpotifyController.kt
+++ src/main/kotlin/com/dewy/musicextractish/spotify/SpotifyController.kt
@@ -1,12 +1,11 @@
 package com.dewy.musicextractish.spotify
 
+import org.springframework.http.ResponseEntity
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.security.web.DefaultRedirectStrategy
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.reactive.function.client.WebClient
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -18,6 +17,39 @@ class SpotifyController(val webClient: WebClient) {
                  response: HttpServletResponse) {
 
         DefaultRedirectStrategy().sendRedirect(request, response, "http://localhost:3000")
+    }
+
+    @CrossOrigin("http://localhost:3000", allowCredentials = "true")
+    @GetMapping("/spotify/user/playlists/export")
+    fun exportPlaylists(
+        @RegisteredOAuth2AuthorizedClient("spotify") authorizedClient: OAuth2AuthorizedClient,
+        @RequestParam(required = false) ids: List<String>?,
+        @RequestParam(required = false) selectAll: Boolean?
+    ): Any {
+        if (selectAll == true) {
+            TODO("Collect all playlists id's and get request info for all of them")
+        }
+
+        if (ids == null || ids.isEmpty()) {
+            return ResponseEntity.badRequest().body("playlistIds is null or empty but should be populated when selectAll is not set")
+        }
+        val playlists = mutableListOf<Any>()
+        ids.forEach {
+            val resourceUri = "https://api.spotify.com/v1/playlists/${it}"
+            val playlist = webClient
+                .get()
+                .uri(resourceUri)
+                .attributes(ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient(authorizedClient))
+                .retrieve()
+                .bodyToMono(Any::class.java)
+                .block()
+            if (playlist != null) {
+                playlists.add(playlist)
+            }
+
+        }
+
+        return playlists
     }
 
     @CrossOrigin("http://localhost:3000", allowCredentials = "true")
